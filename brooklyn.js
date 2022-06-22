9860112e5067ec74cfaad5fab7ad44d9a4bc2e4c
ERANIO KASHARI <98183976+EriKashari@users.noreply.github.com>
2022-06-22 17:47:06 +0200

    Add files via upload

---
+++ brooklyn.js
@@ -0,0 +1,24 @@
+
+const counters = document.querySelectorAll('.counter');
+const speed = 100; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		const inc = target / speed;
+
+
+		if (count < target) {
+		
+			counter.innerText = count + inc;
+		
+			setTimeout(updateCount, 50);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
